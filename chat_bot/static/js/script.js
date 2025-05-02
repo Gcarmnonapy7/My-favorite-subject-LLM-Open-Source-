c59f77a856bbb371a84d7e2e9f6fe26a0208c1e1
Gabriel Carmona <gbcarmona05@gmail.com>
2025-05-02 16:37:42 -0300

    flask implementation

--- chat_bot/static/js/script.js
+++ chat_bot/static/js/script.js
@@ -1,22 +1,23 @@
-//Variaveis para uso
+//Const for use
 const input = document.getElementById("#input");
 const chat = document.getElementById("#chat");
 const send = document.getElementById("#send");
 
 async function sendMessage() {
-  //Se o valor do input == a nulo retornará nada
+  //If the value of the input == "" or null return None
   if (input.value == null|| input.value=="") return;
   let message = input.value;
   input.value = "";
-  //Cria nova bolha de usuário
+  //Creates new user bubble
   let newBubble = CreateUserBubble();
   newBubble.innerHTML = message;
   chat.appendChild(newBubble);
-
+  //Creates new bot bubble
   let newBotBubble = createBotBubble();
   chat.appendChild(newBotBubble);
   chatEnd();
-  newBotBubble.innerHTML = "Carregando...";
+  //Loading message until the server respond and after that the server will pick the awnser of the bot in a JSON.strignify
+  newBotBubble.innerHTML = "Loading...";
   
   const awnser = await fetch ("http://127.0.0.1:5500/chatbot-ETEP/chat_bot/index.html",
     {method : "POST",
@@ -26,12 +27,26 @@ async function sendMessage() {
 };
 
 
-function createUserBubble(){};
+function createUserBubble(){
+  //Create a element p for the user
+  let bubble = document.createElement('p');
+  bubble.classList = "chat_p--user";
+  return bubble;
+};
 
-function createBotBubble(){};
+function createBotBubble(){
+  //Create a element p for the bot
+  let bubble = document.createElement('p');
+  bubble.classList = "chat_p--bot";
+  return bubble
+};
 
-function chatEnd() {};
+function chatEnd() {
+  chat.scrollTop = chat.scrollHeight;
+};
 
+//Activation of the sendMessage when the click on the button happens
+// that will create new bubbles for the user and bot
 send.addEventListener("click",sendMessage);
 input.addEventListener("keyup",function(event){event.preventDefault(); 
   if (event =="Enter"){send.click();}
